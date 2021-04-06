5b2050bc12fb5615453a5cdced17709c8740c3d6
Chetansharma Golla <call2chetan@gmail.com>
2021-04-06 16:18:45 +0200

    push to aws

--- src/api/Constants.js
+++ src/api/Constants.js
@@ -1,5 +1,4 @@
-export const GETSTATIONSURI = "http://127.0.0.1:8000/"; //"http://localhost:8083/v1/getstations";
-//"https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/stations";
+export const GETSTATIONSURI = "http://localhost:8000/";
 export const GETDEPARTURES =
   "https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures?station={stationcode}";
 export const SUBSCRIPTIONKEY = "9501613007cd41398976a63b0a5bd925";
